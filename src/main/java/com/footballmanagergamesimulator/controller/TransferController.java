f24878810349f2b0291f79c194a0f74b3d48b65c
Ciprian Amza <ciprianamza@yahoo.com>
2024-01-20 21:23:35 +0200

    added Transfer endpoints

---
+++ src/main/java/com/footballmanagergamesimulator/controller/TransferController.java
@@ -0,0 +1,33 @@
+package com.footballmanagergamesimulator.controller;
+
+import com.footballmanagergamesimulator.model.Transfer;
+import com.footballmanagergamesimulator.repository.TransferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transfers")
+@CrossOrigin(origins = "http://localhost:4200")
+public class TransferController {
+
+    @Autowired
+    TransferRepository transferRepository;
+
+    @GetMapping("/boughtPlayers/{teamId}/{seasonNumber}")
+    public List<Transfer> getBoughtPlayersByTeamIdAndSeasonId(@PathVariable(name = "teamId") Long teamId, @PathVariable(name = "seasonNumber") Long seasonNumber) { // tactic format: GK=1231&DC=1331&DL=123...
+
+        List<Transfer> boughtPlayers = transferRepository.findAllByBuyTeamIdAndSeasonNumber(teamId, seasonNumber);
+
+        return boughtPlayers;
+    }
+
+    @GetMapping("/soldPlayers/{teamId}/{seasonNumber}")
+    public List<Transfer> getSoldPlayersByTeamIdAndSeasonId(@PathVariable(name = "teamId") Long teamId, @PathVariable(name = "seasonNumber") Long seasonNumber) { // tactic format: GK=1231&DC=1331&DL=123...
+
+        List<Transfer> soldPlayers = transferRepository.findAllBySellTeamIdAndSeasonNumber(teamId, seasonNumber);
+
+        return soldPlayers;
+    }
+}
--- src/main/java/com/footballmanagergamesimulator/repository/TransferRepository.java
+++ src/main/java/com/footballmanagergamesimulator/repository/TransferRepository.java
@@ -4,7 +4,13 @@ import com.footballmanagergamesimulator.model.Transfer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransferRepository extends JpaRepository<Transfer, Long> {
+
+    List<Transfer> findAllByBuyTeamIdAndSeasonNumber(long teamId, long seasonNumber);
+
+    List<Transfer> findAllBySellTeamIdAndSeasonNumber(long teamId, long seasonNumber);
 
 }
