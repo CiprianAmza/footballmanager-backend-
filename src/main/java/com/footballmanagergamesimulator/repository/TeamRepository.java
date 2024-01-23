74f16b86d1927ea7803e11426a40620ce259aab3
Ciprian Amza <ciprianamza@yahoo.com>
2024-01-23 19:47:20 +0200

    added findNameById method

--- src/main/java/com/footballmanagergamesimulator/repository/TeamRepository.java
+++ src/main/java/com/footballmanagergamesimulator/repository/TeamRepository.java
@@ -2,6 +2,7 @@ package com.footballmanagergamesimulator.repository;
 
 import com.footballmanagergamesimulator.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
   List<Team> findAllByCompetitionId(long competitionId);
+
+  @Query("SELECT t.name FROM Team t WHERE t.id = :id")
+  String findNameById(long id);
 }
