adb0aa75d03d871329d0e03c040a2e90345a3e12
ciprian.amza <ciprian.amza@everymatrix.com>
2024-08-08 19:46:05 +0300

    wip

--- src/main/java/com/footballmanagergamesimulator/controller/CompetitionController.java
+++ src/main/java/com/footballmanagergamesimulator/controller/CompetitionController.java
@@ -148,7 +148,7 @@ public class CompetitionController {
     }
 
     @GetMapping("/play")
-    @Scheduled(fixedDelay = 86400000)
+    @Scheduled(fixedDelay = 15000)
     public void play() {
 
         if (round.getRound() == 1) {
