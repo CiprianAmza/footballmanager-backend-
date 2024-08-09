6f732595425924efab2ef06741c866dd8d21e22d
Ciprian Amza <73443976+CiprianAmza@users.noreply.github.com>
2024-08-09 21:22:59 +0300

    Update CompetitionController.java

--- src/main/java/com/footballmanagergamesimulator/controller/CompetitionController.java
+++ src/main/java/com/footballmanagergamesimulator/controller/CompetitionController.java
@@ -148,7 +148,7 @@ public class CompetitionController {
     }
 
     @GetMapping("/play")
-    @Scheduled(fixedDelay = 15000000L)
+    @Scheduled(fixedDelay = 15000L)
     public void play() {
 
         if (round.getRound() == 1 && round.getSeason() == 1) {
