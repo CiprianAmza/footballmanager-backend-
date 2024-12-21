ff2db548c3adaaf88507d91a258960adcd7ba673
Ciprian Amza <73443976+CiprianAmza@users.noreply.github.com>
2024-12-21 02:17:34 +0200

    Update LoginController.java

--- src/main/java/com/footballmanagergamesimulator/controller/LoginController.java
+++ src/main/java/com/footballmanagergamesimulator/controller/LoginController.java
@@ -2,8 +2,10 @@ package com.footballmanagergamesimulator.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class LoginController {
 
     @GetMapping("/login")
