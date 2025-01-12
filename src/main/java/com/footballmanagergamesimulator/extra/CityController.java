b23ba2151bf0118a1bda517d7a8d0fadc4054c05
ciprianamza <ciprianamza@yahoo.com>
2025-01-12 14:41:39 +0200

    partial change

---
+++ src/main/java/com/footballmanagergamesimulator/extra/CityController.java
@@ -0,0 +1,30 @@
+package com.footballmanagergamesimulator.extra;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cities")
+@CrossOrigin(origins = "*")
+public class CityController {
+
+    @Autowired
+    CityRepository cityRepository;
+
+    @GetMapping("/allCities")
+    public List<City> findAllCities() {
+
+        return cityRepository.findAll();
+    }
+
+    @PostMapping("/addCity")
+    public ResponseEntity<City> addCity(@RequestBody City city) {
+
+        City savedCity = cityRepository.save(city);
+        return ResponseEntity.ok(savedCity);
+    }
+}
