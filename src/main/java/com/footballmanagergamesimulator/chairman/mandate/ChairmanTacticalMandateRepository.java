cab503a29cea3f18d7ec31d76b112079a26c5e0a
Ciprian Amza <ciprianamza@yahoo.com>
2026-07-26 14:12:33 +0300

    Load chairman mandate slots with mandate

--- src/main/java/com/footballmanagergamesimulator/chairman/mandate/ChairmanTacticalMandateRepository.java
+++ src/main/java/com/footballmanagergamesimulator/chairman/mandate/ChairmanTacticalMandateRepository.java
@@ -1,9 +1,11 @@
 package com.footballmanagergamesimulator.chairman.mandate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 import java.util.Optional;
 
 public interface ChairmanTacticalMandateRepository extends JpaRepository<ChairmanTacticalMandate, Long> {
+    @EntityGraph(attributePaths = "slots")
     Optional<ChairmanTacticalMandate> findByTeamId(long teamId);
 }
